wbond/subversion

commit f8617d71a780ac4352e986f2dfad05f71f84e1d4

Reintegrate the 1.6.x-issue3683 branch:
 * r964767, r965405, r965469, r965508
   Fix for issue #3683 "Escape unsafe charaters in a URL during export".
   Justification:
     This makes svn export more user friendly, by not forcing the user
     to manually encode the URLs before doing an export.
   Branch:
     The new API code changes in trunk requires a backport branch.
     ^/subversion/branches/1.6.x-issue3683
   Notes:
     r964767 is the actual fix. r965405 is the test for the fix and r965469
     and r965508 are the follow up commits to enhance the fix.
   Votes:
     +1: stylesen, jerenkrantz, lgo


git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.6.x@988064 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/export-cmd.c
@@ -28,6 +28,7 @@
 #include "cl.h"
 
 #include "svn_private_config.h"
+#include "private/svn_opt_private.h"
 
 
 /*** Code. ***/
@@ -67,7 +68,16 @@ svn_cl__export(apr_getopt_t *os,
   if (targets->nelts == 1)
     to = svn_path_uri_decode(svn_path_basename(truefrom, pool), pool);
   else
-    to = APR_ARRAY_IDX(targets, 1, const char *);
+    {
+      to = APR_ARRAY_IDX(targets, 1, const char *);
+
+      /* If given the cwd, pretend we weren't given anything. */
+      if (strcmp("", to) == 0)
+        to = svn_path_uri_decode(svn_path_basename(truefrom, pool), pool);
+      else
+        /* svn_cl__eat_peg_revisions() but only on one target */
+        SVN_ERR(svn_opt__split_arg_at_peg_revision(&to, NULL, to, pool));
+    }
 
   if (! opt_state->quiet)
     svn_cl__get_notifier(&ctx->notify_func2, &ctx->notify_baton2, FALSE, TRUE,
@@ -75,6 +85,9 @@ svn_cl__export(apr_getopt_t *os,
 
   if (opt_state->depth == svn_depth_unknown)
     opt_state->depth = svn_depth_infinity;
+
+  /* Decode the partially encoded URL and escape all URL unsafe characters. */
+  truefrom = svn_path_uri_encode(svn_path_uri_decode(truefrom, pool), pool);
 
   /* Do the export. */
   err = svn_client_export4(NULL, truefrom, to, &peg_revision,

subversion/tests/cmdline/export_tests.py
@@ -398,6 +398,34 @@ def export_HEADplus1_fails(sbox):
                                      'export', sbox.repo_url, sbox.wc_dir,
                                      '-r', 38956)
 
+# This is test for issue #3683 - 'Escape unsafe charaters in a URL during
+# export'
+def export_with_url_unsafe_characters(sbox):
+  "export file with URL unsafe characters"
+
+  ## See http://subversion.tigris.org/issues/show_bug.cgi?id=3683 ##
+
+  sbox.build()
+  wc_dir = sbox.wc_dir
+
+  # Define the paths
+  url_unsafe_path = os.path.join(wc_dir, 'A', 'test- @#$&.txt')
+  url_unsafe_path_url = sbox.repo_url + '/A/test- @#$&.txt@'
+  export_target = os.path.join(wc_dir, 'test- @#$&.txt')
+
+  # Create the file with special name and commit it.
+  svntest.main.file_write(url_unsafe_path, 'This is URL unsafe path file.')
+  svntest.main.run_svn(None, 'add', url_unsafe_path + '@')
+  svntest.actions.run_and_verify_svn(None, None, [], 'ci', '-m', 'log msg',
+                                     '--quiet', wc_dir)
+
+  # Export the file and verify it.
+  svntest.actions.run_and_verify_svn(None, None, [], 'export',
+                                     url_unsafe_path_url, export_target + '@')
+
+  if not os.path.exists(export_target):
+    raise svntest.Failure("export did not fetch file with URL unsafe path")
+
 ########################################################################
 # Run the tests
 
@@ -421,6 +449,7 @@ test_list = [ None,
               export_with_state_deleted,
               export_creates_intermediate_folders,
               export_HEADplus1_fails,
+              export_with_url_unsafe_characters,
              ]
 
 if __name__ == '__main__':

STATUS
@@ -316,17 +316,3 @@ Approved changes:
       checksum isn't used by users of libsvn_ra_serf.
     Votes:
       +1: rhuijben, jerenkrantz (ENOCARE re: 12/39), lgo
-
- * r964767, r965405, r965469, r965508
-   Fix for issue #3683 "Escape unsafe charaters in a URL during export".
-   Justification:
-     This makes svn export more user friendly, by not forcing the user
-     to manually encode the URLs before doing an export.
-   Branch:
-     The new API code changes in trunk requires a backport branch.
-     ^/subversion/branches/1.6.x-issue3683
-   Notes:
-     r964767 is the actual fix. r965405 is the test for the fix and r965469
-     and r965508 are the follow up commits to enhance the fix.
-   Votes:
-     +1: stylesen, jerenkrantz, lgo

subversion/include/private/svn_opt_private.h
@@ -61,7 +61,6 @@ svn_opt__split_arg_at_peg_revision(const char **true_target,
 /* Attempt to transform URL_IN, which is a URL-like user input, into a
  * valid URL:
  *   - escape IRI characters and some other non-URI characters
- *   - check that only valid URI characters remain
  *   - check that no back-path ("..") components are present
  *   - canonicalize the separator ("/") characters
  * URL_IN is in UTF-8 encoding and has no peg revision specifier.

subversion/libsvn_subr/opt.c
@@ -904,12 +904,6 @@ svn_opt__arg_canonicalize_url(const char **url_out, const char *url_in,
   /* Auto-escape some ASCII characters. */
   target = svn_path_uri_autoescape(target, pool);
 
-  /* The above doesn't guarantee a valid URI. */
-  if (! svn_path_is_uri_safe(target))
-    return svn_error_createf(SVN_ERR_BAD_URL, 0,
-                             _("URL '%s' is not properly URI-encoded"),
-                             target);
-
   /* Verify that no backpaths are present in the URL. */
   if (svn_path_is_backpath_present(target))
     return svn_error_createf(SVN_ERR_BAD_URL, 0,

commit 59be02328f4bf741c148ade8c23efd3a51fc7b6b

On the 1.6.x-issue3727 branch:
Fix issue #3727, "svn export into the current directory".
This regression was introduced in r988064. Apparently due to an error
during conflict resolution, parts of r880559 were merged, a revision
which wasn't supposed to be part of the merge.

Also merge the regression test for issue #3727 from trunk.

* subversion/tests/cmdline/export_tests.py
  (export_to_current_dir): Merge this test from trunk and resolve a
   conflict caused by a difference between trunk and 1.6.x in the
   parameter list of run_and_verify_export().
  (test_list): Add new test.

* subversion/svn/export-cmd.c
  (svn_cl__export): Do not special-case the empty path. Fixes the regression.


git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.6.x-issue3727@1032970 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/export-cmd.c
@@ -68,16 +68,9 @@ svn_cl__export(apr_getopt_t *os,
   if (targets->nelts == 1)
     to = svn_path_uri_decode(svn_path_basename(truefrom, pool), pool);
   else
-    {
-      to = APR_ARRAY_IDX(targets, 1, const char *);
+    to = APR_ARRAY_IDX(targets, 1, const char *);
 
-      /* If given the cwd, pretend we weren't given anything. */
-      if (strcmp("", to) == 0)
-        to = svn_path_uri_decode(svn_path_basename(truefrom, pool), pool);
-      else
-        /* svn_cl__eat_peg_revisions() but only on one target */
-        SVN_ERR(svn_opt__split_arg_at_peg_revision(&to, NULL, to, pool));
-    }
+  SVN_ERR(svn_opt__split_arg_at_peg_revision(&to, NULL, to, pool));
 
   if (! opt_state->quiet)
     svn_cl__get_notifier(&ctx->notify_func2, &ctx->notify_baton2, FALSE, TRUE,

subversion/tests/cmdline/export_tests.py
@@ -426,6 +426,37 @@ def export_with_url_unsafe_characters(sbox):
   if not os.path.exists(export_target):
     raise svntest.Failure("export did not fetch file with URL unsafe path")
 
+def export_to_current_dir(sbox):
+  "export to current dir"
+  # Issue 3727: Forced export in current dir creates unexpected subdir.
+  sbox.build(create_wc = False, read_only = True)
+
+  svntest.main.safe_rmtree(sbox.wc_dir)
+  os.mkdir(sbox.wc_dir)
+
+  orig_dir = os.getcwd()
+  os.chdir(sbox.wc_dir)
+
+  export_url = sbox.repo_url + '/A/B/E'
+  export_target = '.'
+  expected_output = svntest.wc.State('', {
+    '.'         : Item(status='A '),
+    'alpha'     : Item(status='A '),
+    'beta'      : Item(status='A '),
+    })
+  expected_disk = svntest.wc.State('', {
+    'alpha'     : Item("This is the file 'alpha'.\n"),
+    'beta'      : Item("This is the file 'beta'.\n"),
+    })
+  svntest.actions.run_and_verify_export(export_url,
+                                        export_target,
+                                        expected_output,
+                                        expected_disk,
+                                        None, None, None, None,
+                                        '--force')
+
+  os.chdir(orig_dir)
+
 ########################################################################
 # Run the tests
 
@@ -450,6 +481,7 @@ test_list = [ None,
               export_creates_intermediate_folders,
               export_HEADplus1_fails,
               export_with_url_unsafe_characters,
+              export_to_current_dir,
              ]
 
 if __name__ == '__main__':